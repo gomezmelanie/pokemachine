44560cf1748cab358845ce4e7e0ed9b14c05fa4a
lounes benzeghioua <lounes.benz@outlook.fr>
2023-11-21 10:37:31 +0100

    maj

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,4 +1,5 @@
 class Pokemon < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  validates :name, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,2 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Pokemon.create(name: "AAAAHHH", level: 15,user_id:4)
+Pokemon.create(name: "BAAAHHH", level:12,user_id:3)
