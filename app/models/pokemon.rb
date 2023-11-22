360d3d4ca60a21a260bfe216d57ecf81f6986461
lounes benzeghioua <lounes.benz@outlook.fr>
2023-11-22 15:06:51 +0100

    waiting

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,4 +1,5 @@
 class Pokemon < ApplicationRecord
   belongs_to :user
   CATEGORIES = ["normal", "feu", "eau", "plante", "electrik", "glace", "combat", "poison", "sol", "vol", "psy", "insecte", "roche", "sprectre","dragon"]
+  POKEMONS = %i[Pikachu Dracaufeu Psykokwak Goupix Rondoudou Bulbizar]
 end
