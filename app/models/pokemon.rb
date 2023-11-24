2cfa43ef79c088581fc1def1cd3502bd7250e55f
virgile popote <virgilepopote@gmail.com>
2023-11-24 12:13:12 +0100

    buttonss

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -2,6 +2,7 @@ class Pokemon < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   belongs_to :user
+  has_many :bookings
   CATEGORIES = ["normal", "feu", "eau", "plante", "electrik", "glace", "combat", "poison", "sol", "vol", "psy", "insecte", "roche", "sprectre","dragon"]
   POKEMONS = %i[Pikachu Dracaufeu Psykokwak Goupix Rondoudou Bulbizar]
 end
