60c38437e1c83292e501b74dd3adf44accb99e1a
melanie gomez vidal <gomezvidal.melanie@gmail.com>
2023-11-24 16:22:15 +0100

    up pokemon

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -4,7 +4,7 @@ class Pokemon < ApplicationRecord
   belongs_to :user
   has_many :bookings
   CATEGORIES = ["normal", "feu", "eau", "plante", "electrik", "glace", "combat", "poison", "sol", "vol", "psy", "insecte", "roche", "sprectre","dragon"]
-  POKEMONS = %i[Abo Abra Aéromite Akwakwak Alakazam Amonistar Amonita Aquali Arbok Arcanin Artikodin Aspicot Boustiflor Carnarticho Caninos Carabaffe Carapuce Chenipan Chétiflor Chrysacier Coconfort Colossinge Crustabri Pikachu Dracaufeu Psykokwak Goupix Rondoudou Bulbizarre]
+  POKEMONS = %i[Abo Abra Aéromite Akwakwak Alakazam Amonistar Amonita Aquali Arbok Arcanin Artikodin Aspicot Boustiflor Carnarticho Caninos Carabaffe Carapuce Chenipan Chétiflor Chrysacier Coconfort Colossinge Crustabri Dardagnan Dodrio Doduo Dracaufeu Draco Dracolosse Pikachu  Psykokwak Goupix Rondoudou Bulbizarre]
   validates :price, presence: true, numericality: { only_float: true }
   validates :level, presence: true, numericality: { only_integer: true }
   validates :address, presence: true
